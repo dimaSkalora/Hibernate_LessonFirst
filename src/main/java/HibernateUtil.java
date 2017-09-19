5cfd7afdf778c2cb1ea556024d0a5eed671d4759
dimaSkalora <timon2@ukr.net>
2017-09-19 13:00:37 +0300

    Java hibernate- Настройка конфигов, урок 1!

--- src/main/java/HibernateUtil.java
+++ src/main/java/HibernateUtil.java
@@ -1,15 +1,16 @@
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
     private static SessionFactory sessionFactory = null;
     static {
-        Configuration cfg = new Configuration().configure();
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
-                .applySettings(cfg.getProperties());
-        sessionFactory = cfg.buildSessionFactory(builder.build());
+        try {
+            //creates the session factory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static SessionFactory getSessionFactory() {
