dc945ec7761543b6ca18e3c55bb5baaa317e984e
dimaSkalora <timon2@ukr.net>
2017-09-19 19:12:42 +0300

    Java hibernate: XML мапинг, связь многие ко многим, урок 5!

---
+++ src/main/java/lesson_5/User.java
@@ -0,0 +1,62 @@
+package lesson_5;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = -5845118151518L;
+    private long id;
+    private int age;
+    private String firstname;
+    private String lastname;
+
+    private Set<Role> roles;
+
+    public User(){
+
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
