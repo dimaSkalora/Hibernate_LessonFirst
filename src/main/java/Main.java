02cda443f69b0cacde385712ac63ada6894fd734
dimaSkalora <timon2@ukr.net>
2017-09-19 11:59:23 +0300

    Java hibernate- Настройка конфигов, урок 1!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
-
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     }
 }
