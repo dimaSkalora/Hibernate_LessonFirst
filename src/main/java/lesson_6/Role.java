793d568449c0c0c6a86d33a77bae6a27db1f05ed
dimaSkalora <timon2@ukr.net>
2017-09-19 19:39:35 +0300

    Java hibernate: Введение в JPA, урок 6!

---
+++ src/main/java/lesson_6/Role.java
@@ -0,0 +1,36 @@
+package lesson_6;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",length = 11)
+    private Long id;
+    @Column(name="title")
+    private String title;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ src/main/java/lesson_6/User.java
@@ -0,0 +1,63 @@
+package lesson_6;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable{
+    private static final long serialVersionUID = -5845118151518L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",length = 11)
+    private long id;
+    @Column(name="age")
+    private int age;
+    @Column(name="firstname", length = 50)
+    private String firstname;
+    @Column(name="lastname")
+    private String lastname;
+
+    public User(){
+
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+}
