04510c3967b69472f0c48e7780a53e03c8da549d
dimaSkalora <timon2@ukr.net>
2017-09-20 10:26:43 +0300

    Java hibernate: JPA связь один к одному, урок 8!

---
+++ src/main/java/lesson_8/User.java
@@ -0,0 +1,73 @@
+package lesson_8;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable{
+    private static final long serialVersionUID = -5845118151518L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",length = 11)
+    private long id;
+    @Column(name="age")
+    private int age;
+    @Column(name = "firstname",length = 50)
+    private String firstname;
+    @Column(name = "lastname", length = 50)
+    private String lastname;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Role role;
+
+    public User(){
+
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
